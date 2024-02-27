b97e85a55fb82637090b5e6d0c84461316183126
MiguelCarapaz <85420759+MiguelCarapaz@users.noreply.github.com>
2024-02-26 20:23:44 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import {
   FaTiktok,
   FaGooglePlay,
@@ -9,38 +9,35 @@ import {
 import { scroller, animateScroll } from "react-scroll";
 
 const Footer = ({ prevPage, nextPage }) => {
-  const anioActual = new Date().getFullYear();
-
-  const handleScrollTo = (elementId) => {
-    scroller.scrollTo(elementId, {
-      duration: 150,
-      delay: 0,
-      smooth: "easeInOutQuart",
-      offset: 1,
-      spy: true,
-    });
-  };
-
-  const handleScrollToTop = () => {
-    animateScroll.scrollToTop({
-      duration: 150,
-      smooth: "easeInOutQuart",
-    });
-  };
+   const anioActual = new Date().getFullYear();
+   const handleScrollTo = (elementId) => {
+     scroller.scrollTo(elementId, {
+       duration: 150,
+       delay: 0,
+       smooth: "easeInOutQuart",
+       offset: 1,
+       spy: true,
+     });
+   };
+   const handleScrollToTop = () => {
+     animateScroll.scrollToTop({
+       duration: 150, // Puedes ajustar la duración según tus preferencias
+       smooth: "easeInOutQuart",
+     });
+   };
 
   return (
     <footer className="bg-slate-100">
-      <div className="container mx-auto py-4">
-        <div className="flex flex-col md:flex-row md:justify-between">
-          <div className="md:flex md:items-center mb-4 md:mb-0">
-            <img
-              src="/assets/logo2.png"
-              alt="logo-saturnina"
-              className="md:mb-0 mb-4"
-            />
-            <div className="flex flex-col md:ml-8">
-              <h4 className="mb-2">Información</h4>
-              <p>
+      <section>
+        <div className="flex flex-col items-center md:flex-row md:items-center md:justify-between p-4">
+          <img
+            src="/assets/logo2.png"
+            alt="logo-saturnina"
+            className="md:mb-0 mb-4"
+          />
+          <div className="flex flex-col justify-center items-center md:mb-0 mb-4">
+            <h4>Información</h4>
+            <p>
                 <a
                   href="https://drive.google.com/file/d/1E8T0iK42wmV79q_J4a8tTZ7f4MNQMx79/view?usp=sharing"
                   target="_blank"
@@ -50,27 +47,28 @@ const Footer = ({ prevPage, nextPage }) => {
                   Términos y Condiciones
                 </a>
               </p>
-              <NavLink
-                to="/dashboard"
-                className="no-underline text-black cursor-pointer mb-2"
-                onClick={() => {
-                  handleScrollToTop();
-                }}
-              >
-              </NavLink>
-              <NavLink
-                to="/dashboard"
-                className="no-underline text-black cursor-pointer"
-                onClick={() => {
-                  handleScrollTo("comentarios");
-                }}
-              >
-              </NavLink>
-            </div>
+            <NavLink
+              to="/dashboard"
+              className="no-underline text-black cursor-pointer"
+              onClick={() => {
+                handleScrollToTop();
+              }}
+            >
+              
+            </NavLink>
+            <NavLink
+              to="/dashboard"
+              className="no-underline text-black cursor-pointer"
+              onClick={() => {
+                handleScrollTo("comentarios");
+              }}
+            >
+              
+            </NavLink>
           </div>
 
           <div className="flex flex-col items-center md:mb-0 mb-4">
-            <h4 className="mb-2">Redes Sociales</h4>
+            <h4>Redes Sociales</h4>
             <div className="flex flex-row text-4xl my-2 gap-4">
             <a href="https://www.tiktok.com/@saturnina.uio?_t=8k1MoBRe8QA&_r=1"
                 target="_blank"
@@ -93,6 +91,7 @@ const Footer = ({ prevPage, nextPage }) => {
             </div>
           </div>
 
+
           <div className="flex flex-col items-center md:mb-0 mb-4">
             <h4 className="mb-2">App Móvil</h4>
             <a href="https://play.google.com/store/apps/details?id=com.saturnina.saturninaapp"
@@ -103,11 +102,11 @@ const Footer = ({ prevPage, nextPage }) => {
             </a>
           </div>
         </div>
-
-        <div className="text-center mt-4">
+        <div className="flex items-center justify-center mt-2 ">
+          <br />
           <p>&copy; {anioActual} Saturnina. Todos los derechos reservados.</p>
         </div>
-      </div>
+      </section>
     </footer>
   );
 };
